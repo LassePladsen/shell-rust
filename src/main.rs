b18d0321c9f45089fc068998c75fd97a9a37622c
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-16 19:22:51 +0100

    Feat: read from stdin

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut input = String::new();
+    let mut cmd: &str = "";
+    while let Ok(_) = io::stdin().read_line(&mut input) {
+        cmd = input.trim();
+        // invalid input
+        println!("{cmd}: command not found");
+    }
 }
