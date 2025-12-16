7af0bd18486b4fe46a4ef05b55531c4f07c7e55d
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-16 19:30:10 +0100

    Feat: repl

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@
 use std::io::{self, Write};
 
 fn main() {
+    // Init
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut input = String::new();
-    let mut cmd: &str = "";
+    let mut cmd: &str;
+
+    // REPL
     while let Ok(_) = io::stdin().read_line(&mut input) {
+
+        // Read
         cmd = input.trim();
+
         // invalid input
         println!("{cmd}: command not found");
+
+        // Restart
+        input.clear();
+        print!("$ ");
+        io::stdout().flush().unwrap();
     }
 }
