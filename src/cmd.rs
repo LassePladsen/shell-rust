7928b8199c53233e383f09fba390fa9a94cd836f
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-17 17:19:32 +0100

    refactor: error and return type

--- src/cmd.rs
+++ src/cmd.rs
@@ -2,62 +2,51 @@ use std::{error, fmt, io};
 
 use crate::env;
 use crate::file;
-use builtin::*;
 
 #[derive(Debug)]
-#[allow(dead_code)]
-pub enum CommandError {
+pub enum CmdError {
     Io(io::Error),
-    ExecutionFailed(String),
+    CmdNotFound(String),
 }
 
-impl fmt::Display for CommandError {
+impl fmt::Display for CmdError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            CommandError::Io(err) => write!(f, "{}", err),
-            CommandError::ExecutionFailed(err) => write!(f, "{}", err),
+            CmdError::Io(err) => write!(f, "{}", err),
+            CmdError::CmdNotFound(err) => write!(f, "{}", err),
         }
     }
 }
 
-impl error::Error for CommandError {
+impl error::Error for CmdError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            CommandError::Io(err) => Some(err),
+            CmdError::Io(err) => Some(err),
             _ => None,
         }
     }
 }
-impl From<io::Error> for CommandError {
+impl From<io::Error> for CmdError {
     fn from(err: io::Error) -> Self {
-        CommandError::Io(err)
+        CmdError::Io(err)
     }
 }
 
 type Args<'a> = Vec<&'a str>;
 type Output = Vec<u8>;
 type Cmd = fn(Args) -> Output;
+type Result<T> = std::result::Result<T, CmdError>;
 
-pub fn run(cmd: &str, args: Args) -> Result<Output, CommandError> {
+pub fn run(cmd: &str, args: Args) -> Result<Output> {
     // Run my builtins
-    if let Some(fn_) = get_cmd_builtin(cmd) {
+    if let Some(fn_) = builtin::get_cmd(cmd) {
         return Ok(fn_(args));
     }
 
-    // Spawn external command
     if let Ok(paths) = env::get_paths()
-        && let Some(_) = get_cmd_path(cmd, paths)
+        && let Ok(output) = spawn_ext_cmd(cmd, args, paths)
     {
-        let mut ext_cmd = std::process::Command::new(cmd);
-        for arg in args {
-            ext_cmd.arg(arg);
-        }
-        let output = ext_cmd.output()?;
-        return Ok(if !output.stdout.is_empty() {
-            output.stdout
-        } else {
-            output.stderr
-        });
+        return Ok(output);
     }
     Ok(notfound(cmd))
 }
@@ -75,6 +64,24 @@ pub fn get_cmd_path(cmd: &str, paths: Vec<String>) -> Option<String> {
     None
 }
 
+pub fn spawn_ext_cmd(cmd: &str, args: Args, paths: Vec<String>) -> Result<Output> {
+    if let Some(_) = get_cmd_path(cmd, paths) {
+        let mut ext_cmd = std::process::Command::new(cmd);
+        for arg in args {
+            ext_cmd.arg(arg);
+        }
+        let output = ext_cmd.output()?;
+        return Ok(if !output.stdout.is_empty() {
+            output.stdout
+        } else {
+            output.stderr
+        });
+    }
+    Err(CmdError::CmdNotFound(
+        format!("Command {cmd} not found in path.").into(),
+    ))
+}
+
 fn notfound(cmd: &str) -> Output {
     format!("{cmd}: not found\n").into()
 }
@@ -83,19 +90,26 @@ pub mod builtin {
     use super::{Args, Cmd, Output};
     use crate::env;
 
-    pub fn get_cmd_builtin(cmd: &str) -> Option<Cmd> {
+    pub fn get_cmd(cmd: &str) -> Option<Cmd> {
         match cmd {
             "type" => Some(type_),
             "echo" => Some(echo),
             "exit" => Some(exit),
+            "pwd" => Some(pwd),
             _ => None,
         }
     }
 
-    fn type_(args: Args) -> Output {
-        let cmd = args.first().expect("Expected a command as argument");
+    fn pwd(args: Args) -> Output {
+        todo!()
+    }
 
-        if get_cmd_builtin(cmd).is_some() {
+    fn type_(args: Args) -> Output {
+        let Some(cmd) = args.first() else {
+            return Vec::default();
+        };
+
+        if get_cmd(cmd).is_some() {
             return format!("{cmd} is a shell builtin\n").into();
         }
 
