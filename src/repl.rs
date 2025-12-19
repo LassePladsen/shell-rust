26f266c2b0dd5edd7db92c568138f02307f0101a
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-19 22:50:18 +0100

    refactor: simplify reading

--- src/repl.rs
+++ src/repl.rs
@@ -10,8 +10,8 @@ pub fn start_repl<R: io::BufRead>(reader: &mut R) {
     let mut buf = String::new();
 
     // Read
-    while let Ok(raw_input) = read_line(reader, &mut buf) {
-        let input = input::parse_input(raw_input);
+    while reader.read_line(&mut buf).is_ok() {
+        let input = input::parse_input(buf.trim());
         let (cmd, args) = (&input[0], input[1..].to_vec());
 
         // Eval
@@ -25,11 +25,6 @@ pub fn start_repl<R: io::BufRead>(reader: &mut R) {
         print!("$ ");
         io::stdout().flush().unwrap();
     }
-}
-
-fn read_line<'a, R: io::BufRead>(reader: &mut R, buf: &'a mut String) -> io::Result<&'a str> {
-    reader.read_line(buf)?;
-    Ok(buf.trim())
 }
 
 fn eval(cmd: &str, args: Input) -> String {
