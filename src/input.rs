12040a1ed17d3ec25faf084c60e69ab08467cdd3
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-19 19:49:47 +0100

    tmp checkpoint

--- src/input.rs
+++ src/input.rs
@@ -1,5 +1,34 @@
 pub type Args = Vec<String>;
 
+#[derive(Debug)]
+enum RawArgKind {
+    SingleQuote,
+    DoubleQuote,
+    Space,
+    Literal, // everything else, aka. normal string
+}
+use RawArgKind::*;
+
+#[derive(Debug)]
+struct RawArg {
+    kind: RawArgKind,
+    content: String,
+}
+
+#[derive(Debug)]
+struct Single
+
+impl RawArg {
+    fn push_char(&self, ch: char) {
+        match self.kind {
+            SingleQuote: 
+        }
+    }
+    
+}
+
+type RawArgs = Vec<RawArg>;
+
 /// Returns (cmd, vec of args)
 pub fn parse_input(input: &str) -> (&str, Args) {
     if input.is_empty() {
@@ -11,59 +40,86 @@ pub fn parse_input(input: &str) -> (&str, Args) {
     }
 }
 
-/// Handles quotes
 fn parse_args(args: &str) -> Args {
     if args.is_empty() {
         return Default::default();
     }
+    resolve_raw_args(collect_raw_args(args))
+}
 
-    let mut output = Default::default();
-
-    // Init character loop
-    let mut chars = args.chars();
-    let Some(mut prev_ch) = chars.next() else {
-        return output;
-    };
-    let mut in_quotes = false;
-    let mut word = String::with_capacity(args.len());
-    if '\'' == prev_ch {
-        in_quotes = true;
-    } else {
-        word.push(prev_ch);
+fn collect_raw_args(args: &str) -> RawArgs {
+    if args.is_empty() {
+        return Default::default();
     }
 
-    // Loop through each character until we find connecting single quotes to collect into words
-    // Don't include the quotes themselves
+    let mut chars = args.chars();
+    let mut output: RawArgs = Default::default();
+
+    if args.len() == 1 {
+        // Treat as Literal. Doesn't matter if its technically whitespace.
+        output.push(RawArg {
+            kind: RawArgKind::Literal,
+            content: chars
+                .next()
+                .expect("Should be one char in this for sure")
+                .into(),
+        });
+        return output;
+    }
+
+    // Loop through each character, start a single/double quoted section until the next unescaped
+    // corresponding quote. Don't include the quotes themselves except if inside a quoted section
+    // or escaped.
+    let mut kind: Option<RawArgKind> = None;
+    let mut raw_arg: RawArg;
     for ch in chars {
-        // println!();
-        // dbg!(in_quotes);
-        // dbg!(&word);
-        // dbg!(&output);
-        // dbg!(&prev_ch);
-        // dbg!(&ch);
-        match ch {
+        dbg!(&ch);
+
+        // Between args
+        if kind.is_none() {
+            raw_arg = RawArg {
+                kind: match ch {
+                    '\'' => SingleQuote,
+                    '"' => DoubleQuote,
+                    ' ' => Space,
+                    _ => Literal,
+                },
+                content: Default::default(),
+            };
+            continue;
+        }
+
+        raw_arg.push_char(ch);
+
+
+
+
+
+
+
+        {
             ' ' => {
                 // If not in a quoted word, then end current word on space
-                if !in_quotes && !word.is_empty() {
-                    output.push(word.clone());
-                    word.clear();
-                } 
+                if !in_quotes && !arg_buf.is_empty() {
+                    output.push(arg_buf.clone());
+                    arg_buf.clear();
+                }
                 // Strip space between nonquoted words
-                else if !word.is_empty() {
-                    word.push(ch);
-                } 
+                else if !arg_buf.is_empty() {
+                    arg_buf.push(ch);
+                }
             }
             '\'' => {
                 // End of quoted word
                 if in_quotes {
-                    output.push(word.clone());
-                    word.clear();
+                    output.push(arg_buf.clone());
+                    arg_buf.clear();
                     in_quotes = false;
                 }
                 // Make sure to concat if previous was a quoted word, by removing from output and
                 // starting to concat this new word
-                else if '\'' == prev_ch && word.is_empty() {
-                    word = output.pop().unwrap_or("".to_string());
+                else if '\'' == prev_ch && arg_buf.is_empty() {
+                    arg_buf = output.pop().unwrap_or("".to_string());
                     in_quotes = true;
                 }
                 // Start of new quoted word
@@ -72,16 +128,20 @@ fn parse_args(args: &str) -> Args {
                     in_quotes = true;
                 }
             }
-            _ => word.push(ch),
+            _ => arg_buf.push(ch),
         }
 
         prev_ch = ch;
     }
 
     // Add the final word
-    if !word.is_empty() {
-        output.push(word);
+    if !arg_buf.is_empty() {
+        output.push(arg_buf);
     }
 
     output
+}
+
+fn resolve_raw_args(raw_args: RawArgs) -> Args {
+    todo!()
 }
