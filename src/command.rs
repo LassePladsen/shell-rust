6e9c09022563a49b9cd58337a120310fb6d5722d
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-01-03 16:23:07 +0100

    fix: appending output like 'tail -f' by spawning instead of using .output

--- src/command.rs
+++ src/command.rs
@@ -129,9 +129,12 @@ pub fn cmd_in_paths(cmd: &str, paths: &[String]) -> bool {
 }
 
 pub fn spawn_ext_cmd(cmd: &str, args: ArgsSlice, pipe_output: Option<Output>) -> Result<Output> {
-    // TODO: support appended output like 'tail -f'. Need to use spawn instead of output
     if pipe_output.is_none() {
-        return Ok(std::process::Command::new(cmd).args(args).output()?.into());
+        return Ok(std::process::Command::new(cmd)
+            .args(args)
+            .spawn()?
+            .wait_with_output()?
+            .into());
     }
 
     // From here on out this is a piped command
