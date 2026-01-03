eaf48bd1fe297da67456deb3adc07c94b0aac19c
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-01-03 17:10:14 +0100

    revert: fix: appending/blocking output. add todo

--- src/command.rs
+++ src/command.rs
@@ -127,13 +127,10 @@ pub fn cmd_in_paths(cmd: &str, paths: &[String]) -> bool {
 }
 
 pub fn spawn_ext_cmd(cmd: &str, args: ArgsSlice, pipe_output: Option<Output>) -> Result<Output> {
+    // TODO: support blocking outputs, like 'tail -f'. This is done by using stdout =
+    // Stdio::inhreit(), but then piping/redirecting wont work (needs stdout = Stdio::piped())
     if pipe_output.is_none() {
-        return Ok(std::process::Command::new(cmd)
-            .args(args)
-            .stdout(Stdio::piped())
-            .spawn()?
-            .wait_with_output()?
-            .into());
+        return Ok(std::process::Command::new(cmd).args(args).output()?.into());
     }
 
     // From here on out this is a piped command
